57f5c1cfa9033ea8fbbad8eee4460cd95211b596
Anshul <anshulusr@gmail.com>
2023-08-14 22:37:19 +0530

    implemented string output

--- src/main.rs
+++ src/main.rs
@@ -21,29 +21,57 @@ fn main() {
         }
     };
     let mut image = Vec::<u8>::new();
-    // mov ah, 0x09
-    // Instructs BIOS to Write a character
-    image.push(0xb4);
-    image.push(0x09);
-    // mov al, 0x41 aka 'A'
-    // What character to print
-    image.push(0xb0);
-    image.push(0x41);
-    // mov cl, 0x05
-    // How many times should a character be printed
-    image.push(0xb1);
-    image.push(0x01);
-    // mov bl, 0x01
-    // Sets color of text
-    image.push(0xb3);
-    image.push(0x0f);
-    // mov bh, 0x01
-    // Which page number to display the character on
-    image.push(0xb7);
-    image.push(0x00);
-    // int 0x10
-    image.push(0xcd);
-    image.push(0x10);
+
+    let mut print_char = |position, char| {
+        // mov ah, 0x09
+        // Instructs BIOS to change position of cursor
+        image.push(0xb4);
+        image.push(0x02);
+        // mov bh, 0x01
+        // Which page number to set the cursor to
+        image.push(0xb7);
+        image.push(0x00);
+        // mov dh, [position]
+        // Sets the row for the cursor
+        image.push(0xb6);
+        image.push(0x00);
+        // mov dl, 0x00
+        // Sets the column for the cursor
+        image.push(0xb2);
+        image.push(position);
+        // int 0x10
+        image.push(0xcd);
+        image.push(0x10);
+        // mov ah, 0x09
+        // Instructs BIOS to Write a character
+        image.push(0xb4);
+        image.push(0x09);
+        // mov al, [Input character in ASCII]
+        // What character to print
+        image.push(0xb0);
+        image.push(char as u8);
+        // mov cl, 0x05
+        // How many times should a character be printed
+        image.push(0xb1);
+        image.push(0x01);
+        // mov bl, 0x01
+        // Sets color of text
+        image.push(0xb3);
+        image.push(0x0f);
+        // mov bh, 0x01
+        // Which page number to display the character on
+        image.push(0xb7);
+        image.push(0x00);
+        // int 0x10
+        image.push(0xcd);
+        image.push(0x10);
+    };
+    "Hello World!"
+        .chars()
+        .enumerate()
+        .for_each(|(position, char)| {
+            print_char(position as u8, char);
+        });
     let len = image.len();
     // Stuff 0s till 510th byte
     (len..0x1fe).for_each(|_| image.push(0));
