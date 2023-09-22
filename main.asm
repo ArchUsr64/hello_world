267df1032cbadb057558176deeb1af7e0499095e
Anshul Dalal <anshulusr@gmail.com>
2023-09-22 11:43:46 +0530

    Implemented Hello World with memory stored data

--- main.asm
+++ main.asm
@@ -1,28 +1,22 @@
+[org 0x7c00]
+mov ah, 0x0e
+mov bx, string
+	cmp al, 0
+	je end
 start:
-	mov cl, 'A'
-alphs:
-	mov al, cl
-	and al, 0x1
-	mov al, cl
-	jnz skip_case_change
-	or al, 0x20
-skip_case_change:
-	call write_char
-	inc cl
-	cmp cl, 'Z' + 1
-	jne alphs
-	jmp end
-
-write_char:
-	mov bh, 0x00
-	mov bl, 0x0f
-	mov ah, 0x0e
+	mov al, [bx]
+	cmp al, 0
+	je end
 	int 0x10
-	ret
+	inc bx
+	jmp start
 
 end:
 	nop
 	jmp end
 
-DB 510-($-start) DUP(0)
+string:
+	DB "Hello, World!", 0
+
+DB 510-($-$$) DUP(0)
 DW 0xaa55
