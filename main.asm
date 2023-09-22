df0e7f588925ffae030b7a48a1194bfc32791c8a
Anshul Dalal <anshulusr@gmail.com>
2023-09-22 19:01:17 +0530

    Implemented data reading from second sector

--- main.asm
+++ main.asm
@@ -1,77 +1,59 @@
 [org 0x7c00]
-str_len EQU 10
-line_feed EQU 0x0a
-carriage_feed EQU 0x0D
-enter_scancode EQU 0x1c
+LINE_FEED EQU 0x0a
+CARRIAGE_FEED EQU 0x0D
+SECOND_SECTOR_LOCATION EQU 0x7e00
 
 start:
-	mov ah, 0x0e
-	mov si, input_prompt
+	; Store disk num from BIOS
+	mov [DISK_NUM], dl
+
+	; Set Extra segment to 0
+	mov ax, 0
+	mov es, ax
+
+	mov ah, 0x02
+	mov al, 0x01
+	mov ch, 0x00
+	mov cl, 0x02
+	mov dh, 0x00
+	mov dl, [DISK_NUM]
+	mov bx, SECOND_SECTOR_LOCATION
+	int 0x13
+	jc handle_disk_errors
+
+	mov si, SECOND_SECTOR_LOCATION
 	call print_string
+	jmp end
 
-echo_mode:
-	mov bx, input_buffer
-echo_loop:
-	mov ah, 0
-	int 0x16
-	cmp ah, enter_scancode
+print_string:
+	; Expects the pointer to string in si
 	mov ah, 0x0e
-	int 0x10
-	je output
-	mov [bx], al
-	inc bx
-	jmp echo_loop
-
-output:
-	mov ah, 0x0e
-	mov al, line_feed
-	int 0x10
-	mov si, output_prompt
-	call print_string
-
-print_inverted_case:
-	mov bx, input_buffer
-print_inverted_case_loop:
-	mov al, [bx]
+print_string_loop:
+	mov al, [si]
 	cmp al, 0
-	je restart
-	xor al, 0x20
+	je print_string_return
 	int 0x10
-	inc bx
-	jmp print_inverted_case_loop
+	inc si
+	jmp print_string_loop
+print_string_return:
+	ret
 
-restart:
-	mov ah, 0x0e
-	mov al, line_feed
-	mov ah, 0x0e
-	int 0x10
-	mov al, 13
-	int 0x10
-	jmp start
+handle_disk_errors:
+	mov si, FAIL_MESSAGE
+	jmp end
+	
 
 end:
 	nop
 	jmp end
 
-print_string:
-	; Expects the pointer to string in si
-	mov al, [si]
-	cmp al, 0
-	je return
-	int 0x10
-	inc si
-	jmp print_string
-return:
-	ret
+DISK_NUM:
+	DB 0
 
-input_prompt:
-	DB "Enter string: ", 0
-
-output_prompt:
-	DB "Toggled Case: ", 0
-
-input_buffer:
-	TIMES str_len DB 0
+FAIL_MESSAGE:
+	DB "Failed to read second disk sector, ERROR CODE: 0x[TODO!]", 0
 
 DB 510-($-$$) DUP(0)
 DW 0xaa55
+
+DB "String from other sector", 0
